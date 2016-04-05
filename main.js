e5cd14ea5230f1b58ce6f7b65bf5220ea60358d2
gemah <hianneiva@gmail.com>
2016-04-05 19:06:54 -0300

    Personal Site
    Added responsive mobile CSS (v1). Adjusted minor details on About me
    text.

--- main.js
+++ main.js
@@ -14,3 +14,22 @@ function reveal(divID){
 	divs[divID].className = "schrodingerAlive";
 	
 }
+
+function altBG(BGid){
+	
+	if(BGid < 1){
+			document.body.style.backgroundColor = "teal";
+			document.getElementById("sideMenu").style.backgroundColor = "#006060";
+			document.getElementById("fixedText").style.display = "block";
+	}
+	if(BGid == 1){
+		document.body.style.backgroundColor = "#8B008B";
+		document.getElementById("sideMenu").style.backgroundColor = "#710071";
+		document.getElementById("fixedText").style.display = "none";
+	}
+	if(BGid > 1){
+		document.body.style.backgroundColor = "#D58E00";
+		document.getElementById("sideMenu").style.backgroundColor = "#AE7400";
+		document.getElementById("fixedText").style.display = "none";
+	}
+}
